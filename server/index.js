519ea103dd120cff9ec1b8ddf21e7fde7ce21999
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-03-29 16:30:08 +0530

    adding routes

--- server/index.js
+++ server/index.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+
+const routes = [
+  {
+    bus_no: "500A",
+    stops: ["Silk Board", "BTM", "Jayanagar", "Majestic"]
+  },
+  {
+    bus_no: "201",
+    stops: ["Electronic City", "Silk Board", "HSR", "Koramangala"]
+  }
+];
